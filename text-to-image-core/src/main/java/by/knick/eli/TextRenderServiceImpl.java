1533253e32ce08c1b8b72295d123f4f89428d113
Kirill Nikolaenko <knikolaenko@elinext.com>
2016-11-10 00:06:47 +0300

    Implemented text rendering in Arial 48px font

--- text-to-image-core/src/main/java/by/knick/eli/TextRenderServiceImpl.java
+++ text-to-image-core/src/main/java/by/knick/eli/TextRenderServiceImpl.java
@@ -1,9 +1,49 @@
 package by.knick.eli;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class TextRenderServiceImpl implements TextRenderService {
 
 	@Override
 	public byte[] renderText(String text) {
-		return new byte[0];
+		BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2d = img.createGraphics();
+
+		// Set font
+		Font font = new Font("Arial", Font.PLAIN, 48);
+		g2d.setFont(font);
+
+		// Calculate image size with selected font
+		FontMetrics fm = g2d.getFontMetrics();
+		int width = fm.stringWidth(text);
+		int height = fm.getHeight();
+		g2d.dispose();
+
+		// Set rendering settings
+		img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		g2d = img.createGraphics();
+		g2d.setFont(font);
+
+		// Draw text
+		fm = g2d.getFontMetrics();
+		g2d.setColor(Color.BLACK);
+		g2d.drawString(text, 0, fm.getAscent());
+		g2d.dispose();
+
+		// Write to byte array
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			ImageIO.write(img, "png", baos);
+			return baos.toByteArray();
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
 	}
 }
--- text-to-image-core/src/test/java/by/knick/eli/TextRenderServiceTest.java
+++ text-to-image-core/src/test/java/by/knick/eli/TextRenderServiceTest.java
@@ -4,16 +4,19 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TextRenderServiceTest {
 
 	private TextRenderService testable = new TextRenderServiceImpl();
-	
+
 	@Test
-	public void renderHello() throws IOException{
+	public void renderHello() throws IOException {
 		byte[] imgBytes = testable.renderText("Hello World");
-		File imgBytesFile = File.createTempFile("text-render-unittest-renderHello", ".png");
+		Assert.assertNotNull(imgBytes);
+
+		File imgBytesFile = File.createTempFile("text-render-unittest-renderHello-", ".png");
 		FileUtils.writeByteArrayToFile(imgBytesFile, imgBytes);
 	}
 }
