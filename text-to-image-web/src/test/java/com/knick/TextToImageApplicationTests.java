9a1d5d3bb8274e494fb56f260f0cacfa47e40d9d
Kirill Nikolaenko <knikolaenko@elinext.com>
2016-11-09 23:25:07 +0300

    Project init

---
+++ text-to-image-web/src/test/java/com/knick/TextToImageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.knick;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TextToImageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
