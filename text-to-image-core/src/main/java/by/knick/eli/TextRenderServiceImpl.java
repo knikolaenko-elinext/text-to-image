b4143ae66b37b1fbb7c5279ff171a03cfc6975ac
knikolaenko <kir.nikolaenko@gmail.com>
2016-11-10 13:32:51 +0300

    Added Calculation Font logic to fix size

--- text-to-image-core/src/main/java/by/knick/eli/TextRenderServiceImpl.java
+++ text-to-image-core/src/main/java/by/knick/eli/TextRenderServiceImpl.java
@@ -1,41 +1,88 @@
 package by.knick.eli;
 
 import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.font.FontRenderContext;
+import java.awt.font.LineBreakMeasurer;
+import java.awt.font.TextAttribute;
+import java.awt.font.TextLayout;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.text.AttributedCharacterIterator;
+import java.text.AttributedString;
+import java.util.Hashtable;
 
 import javax.imageio.ImageIO;
 
 public class TextRenderServiceImpl implements TextRenderService {
 
+	private static final int PREFFERABLE_WIDTH = 198;
+	private static final int PREFFERABLE_HEIGHT_SHORT = 68;
+	private static final int PREFFERABLE_HEIGHT_TALL = 198;
+
+	private static final int MINIMAL_FONT_SIZE = 8;
+	private static final int MAXIMUM_FONT_SIZE = PREFFERABLE_HEIGHT_SHORT;
+
+	private static final double THRESHOLD = 0.97;
+	private static final int MAX_ITERATIONS = 15;
+
 	@Override
 	public byte[] renderText(String text) {
-		BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+		SizeCalculationResult sizeCalculationResult = calculateFontSize(text);
+
+		float fontSize = sizeCalculationResult.fontSize;
+		boolean useTallVersion = sizeCalculationResult.useTallVersion;
+
+		Hashtable<TextAttribute, Object> textAttributes = new Hashtable<TextAttribute, Object>();
+		textAttributes.put(TextAttribute.FAMILY, "Serif");
+		textAttributes.put(TextAttribute.SIZE, new Float(fontSize));
+		AttributedString attributedText = new AttributedString(text, textAttributes);
+
+		// Create a new LineBreakMeasurer from the paragraph.
+		AttributedCharacterIterator paragraph = attributedText.getIterator();
+		// index of the first character in the paragraph.
+		int paragraphStart = paragraph.getBeginIndex();
+		// index of the first character after the end of the paragraph.
+		int paragraphEnd = paragraph.getEndIndex();
+
+		BufferedImage img = new BufferedImage(PREFFERABLE_WIDTH, useTallVersion ? PREFFERABLE_HEIGHT_TALL : PREFFERABLE_HEIGHT_SHORT, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2d = img.createGraphics();
+		setRenderingHints(g2d);
+		FontRenderContext frc = g2d.getFontRenderContext();
 
-		// Set font
-		Font font = new Font("Arial", Font.PLAIN, 48);
-		g2d.setFont(font);
+		LineBreakMeasurer lineMeasurer = new LineBreakMeasurer(paragraph, frc);
 
-		// Calculate image size with selected font
-		FontMetrics fm = g2d.getFontMetrics();
-		int width = fm.stringWidth(text);
-		int height = fm.getHeight();
-		g2d.dispose();
+		// Set break width to width of Component.
+		float breakWidth = PREFFERABLE_WIDTH;
+		float drawPosY = 0;
+		// Set position to the index of the first character in the paragraph.
+		lineMeasurer.setPosition(paragraphStart);
 
-		// Set rendering settings
-		img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		g2d = img.createGraphics();
-		g2d.setFont(font);
+		// Get lines until the entire paragraph has been displayed.
+		while (lineMeasurer.getPosition() < paragraphEnd) {
 
-		// Draw text
-		fm = g2d.getFontMetrics();
-		g2d.setColor(Color.BLACK);
-		g2d.drawString(text, 0, fm.getAscent());
+			// Retrieve next layout. A cleverer program would also cache
+			// these layouts until the component is re-sized.
+			TextLayout layout = lineMeasurer.nextLayout(breakWidth);
+
+			// Compute pen x position. If the paragraph is right-to-left we
+			// will align the TextLayouts to the right edge of the panel.
+			// Note: this won't occur for the English text in this sample.
+			// Note: drawPosX is always where the LEFT of the text is placed.
+			float drawPosX = layout.isLeftToRight() ? 0 : breakWidth - layout.getAdvance();
+
+			// Move y-coordinate by the ascent of the layout.
+			drawPosY += layout.getAscent();
+
+			// Draw the TextLayout at (drawPosX, drawPosY).
+			layout.draw(g2d, drawPosX, drawPosY);
+
+			// Move y-coordinate in preparation for next layout.
+			drawPosY += layout.getDescent() + layout.getLeading();
+		}
+
 		g2d.dispose();
 
 		// Write to byte array
@@ -45,5 +92,140 @@ public class TextRenderServiceImpl implements TextRenderService {
 		} catch (IOException ex) {
 			throw new RuntimeException(ex);
 		}
+	}
+
+	private static final class SizeCalculationResult {
+		float fontSize;
+		boolean useTallVersion;
+
+		public SizeCalculationResult(float fontSize, boolean useTallVersion) {
+			super();
+			this.fontSize = fontSize;
+			this.useTallVersion = useTallVersion;
+		}
+	}
+
+	SizeCalculationResult calculateFontSize(String text) {
+		BufferedImage img = new BufferedImage(PREFFERABLE_WIDTH, PREFFERABLE_HEIGHT_TALL, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2d = img.createGraphics();
+		setRenderingHints(g2d);
+		FontRenderContext frc = g2d.getFontRenderContext();
+
+		float currentFontSize = MINIMAL_FONT_SIZE;
+
+		Hashtable<TextAttribute, Object> textAttributes = new Hashtable<TextAttribute, Object>();
+		textAttributes.put(TextAttribute.FAMILY, "Serif");
+
+		boolean fontSizeFound = false;
+		boolean useTallVersion = false;
+		boolean didDecreasingOnLastIteration = false;
+		int multiplyFactor = 2;
+		int iterationNumber = 0;
+
+		while (!fontSizeFound) {
+			iterationNumber++;
+			textAttributes.put(TextAttribute.SIZE, new Float(currentFontSize));
+
+			AttributedString attributedText = new AttributedString(text, textAttributes);
+
+			AttributedCharacterIterator paragraph = attributedText.getIterator();
+			// Create a new LineBreakMeasurer from the paragraph.
+			LineBreakMeasurer lineMeasurer = new LineBreakMeasurer(paragraph, frc);
+			// Set position to the index of the first character in the
+			// paragraph.
+			lineMeasurer.setPosition(paragraph.getBeginIndex());
+
+			float drawPosY = 0;
+			// Get lines until the entire paragraph has been displayed.
+			while (lineMeasurer.getPosition() < paragraph.getEndIndex()) {
+				// Retrieve next row layout
+				TextLayout layout = lineMeasurer.nextLayout(PREFFERABLE_WIDTH);
+				// Move y-coordinate by the font size
+				drawPosY += layout.getAscent() + layout.getDescent() + layout.getLeading();
+			}
+
+			if (!useTallVersion) {
+				float hitRateForShortVersion = drawPosY / PREFFERABLE_HEIGHT_SHORT;
+				if (hitRateForShortVersion <= THRESHOLD) {
+					// Need to increase font
+					if (currentFontSize >= MAXIMUM_FONT_SIZE) {
+						// No chance to increase - use max font
+						currentFontSize = MAXIMUM_FONT_SIZE;
+						fontSizeFound = true;
+					} else {
+						if (didDecreasingOnLastIteration) {
+							multiplyFactor++;
+						}
+						if (iterationNumber >= MAX_ITERATIONS) {
+							fontSizeFound = true;
+							break;
+						}
+						currentFontSize /= Math.pow(hitRateForShortVersion, (1.0 / multiplyFactor));
+						didDecreasingOnLastIteration = false;
+					}
+				} else if (hitRateForShortVersion > THRESHOLD && hitRateForShortVersion <= 1) {
+					// Win!
+					fontSizeFound = true;
+				} else if (hitRateForShortVersion > 1) {
+					// Need to decrease font
+					if (currentFontSize <= MINIMAL_FONT_SIZE) {
+						// No way to fit short version, go to tall
+						useTallVersion = true;
+					} else {
+						currentFontSize /= Math.pow(hitRateForShortVersion, (1.0 / multiplyFactor));
+						didDecreasingOnLastIteration = true;
+					}
+				}
+			}
+			if (useTallVersion) {
+				float hitRateForTallVersion = drawPosY / PREFFERABLE_HEIGHT_TALL;
+				if (hitRateForTallVersion <= THRESHOLD) {
+					// Need to increase font
+					if (currentFontSize >= MAXIMUM_FONT_SIZE) {
+						// No chance to increase - use max font
+						currentFontSize = MAXIMUM_FONT_SIZE;
+						fontSizeFound = true;
+					} else {
+						if (iterationNumber >= MAX_ITERATIONS) {
+							fontSizeFound = true;
+							break;
+						}
+						if (didDecreasingOnLastIteration) {
+							multiplyFactor++;
+						}
+						currentFontSize /= Math.pow(hitRateForTallVersion, (1.0 / multiplyFactor));
+						didDecreasingOnLastIteration = false;
+					}
+				} else if (hitRateForTallVersion > THRESHOLD && hitRateForTallVersion <= 1) {
+					// Win!
+					fontSizeFound = true;
+				} else if (hitRateForTallVersion > 1) {
+					// Need to decrease font
+					if (currentFontSize <= MINIMAL_FONT_SIZE) {
+						// No chance to decrease - use min font
+						currentFontSize = MINIMAL_FONT_SIZE;
+						fontSizeFound = true;
+					} else {
+						currentFontSize /= Math.pow(hitRateForTallVersion, (1.0 / multiplyFactor));
+						didDecreasingOnLastIteration = true;
+					}
+				}
+			}
+		}
+		g2d.dispose();
+
+		return new SizeCalculationResult(currentFontSize, useTallVersion);
+	}
+
+	private void setRenderingHints(Graphics2D g2d) {
+		g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+		g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+		g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+		g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+		g2d.setColor(Color.BLACK);
 	}
 }
--- text-to-image-core/src/test/java/by/knick/eli/TextRenderServiceTest.java
+++ text-to-image-core/src/test/java/by/knick/eli/TextRenderServiceTest.java
@@ -11,12 +11,21 @@ public class TextRenderServiceTest {
 
 	private TextRenderService testable = new TextRenderServiceImpl();
 
-	@Test
-	public void renderHello() throws IOException {
-		byte[] imgBytes = testable.renderText("Hello World");
-		Assert.assertNotNull(imgBytes);
+	private String textExample = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero sapien, lacinia vitae aliquet eu, interdum quis velit. Vivamus ullamcorper nibh non tincidunt faucibus. Ut vel nisi at dui elementum ornare. Fusce nec dictum est, sed semper mi. Cras eget enim eget sem rhoncus eleifend. Nulla facilisi. Donec ut nulla arcu. Quisque tempor semper mi. Etiam commodo tincidunt sodales. Vivamus vulputate est vel nisi finibus, et mattis ex pellentesque. Proin eget urna nec leo auctor condimentum nec quis erat. Sed condimentum neque arcu, a laoreet mi imperdiet vitae. Maecenas rhoncus augue non rutrum tempor. Aliquam at venenatis augue, et lacinia nunc. Aliquam erat volutpat. Nullam at sem vehicula, laoreet eros ac, malesuada dolor. Integer molestie tincidunt tortor at semper. Cras euismod condimentum tortor, vitae cursus nulla gravida eget. Praesent nulla mi, rhoncus ultricies mi in, ultricies ultricies nisi. Proin consequat nisl id ultrices pretium. Cras in elit vel nibh ullamcorper volutpat vel in ipsum. Praesent porttitor dignissim sem ut scelerisque. Suspendisse ut pellentesque lectus. Proin sit amet fermentum justo. Aliquam sodales at justo sit amet ultrices. Mauris viverra, turpis vulputate convallis laoreet, odio tortor finibus odio, in porta mauris arcu at leo.";
 
-		File imgBytesFile = File.createTempFile("text-render-unittest-renderHello-", ".png");
-		FileUtils.writeByteArrayToFile(imgBytesFile, imgBytes);
+	@Test
+	public void render() throws IOException {
+		int charCount = 1;
+		do {
+			String text = textExample.substring(0, charCount);
+
+			byte[] imgBytes = testable.renderText(text);
+			Assert.assertNotNull(imgBytes);
+
+			File imgBytesFile = File.createTempFile("text-render-unittest-renderHello-", ".png");
+			FileUtils.writeByteArrayToFile(imgBytesFile, imgBytes);
+
+			charCount = (int)(charCount * 1.5) + 1;
+		} while (charCount <= textExample.length());
 	}
 }
