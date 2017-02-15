587ce1055d97cb1a47625dbc7a78c9dc2ed83c11
knikolaenko <knikolaenko@elinext.com>
2017-02-15 12:25:36 +0300

    Added 'UTF-8' characters unit-test

--- text-to-image-core/src/test/java/by/knick/eli/TextRenderServiceTest.java
+++ text-to-image-core/src/test/java/by/knick/eli/TextRenderServiceTest.java
@@ -14,7 +14,7 @@ public class TextRenderServiceTest {
 	private String textExample = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero sapien, lacinia vitae aliquet eu, interdum quis velit. Vivamus ullamcorper nibh non tincidunt faucibus. Ut vel nisi at dui elementum ornare. Fusce nec dictum est, sed semper mi. Cras eget enim eget sem rhoncus eleifend. Nulla facilisi. Donec ut nulla arcu. Quisque tempor semper mi. Etiam commodo tincidunt sodales. Vivamus vulputate est vel nisi finibus, et mattis ex pellentesque. Proin eget urna nec leo auctor condimentum nec quis erat. Sed condimentum neque arcu, a laoreet mi imperdiet vitae. Maecenas rhoncus augue non rutrum tempor. Aliquam at venenatis augue, et lacinia nunc. Aliquam erat volutpat. Nullam at sem vehicula, laoreet eros ac, malesuada dolor. Integer molestie tincidunt tortor at semper. Cras euismod condimentum tortor, vitae cursus nulla gravida eget. Praesent nulla mi, rhoncus ultricies mi in, ultricies ultricies nisi. Proin consequat nisl id ultrices pretium. Cras in elit vel nibh ullamcorper volutpat vel in ipsum. Praesent porttitor dignissim sem ut scelerisque. Suspendisse ut pellentesque lectus. Proin sit amet fermentum justo. Aliquam sodales at justo sit amet ultrices. Mauris viverra, turpis vulputate convallis laoreet, odio tortor finibus odio, in porta mauris arcu at leo.";
 
 	@Test
-	public void render() throws IOException {
+	public void renderDifferentLength() throws IOException {
 		int charCount = 1;
 		do {
 			String text = textExample.substring(0, charCount);
@@ -27,5 +27,14 @@ public class TextRenderServiceTest {
 
 			charCount = (int)(charCount * 1.3) + 1;
 		} while (charCount <= textExample.length());
+	}
+	
+	@Test
+	public void renderNonUtf() throws IOException {
+		byte[] imgBytes = testable.renderText("β-Test", "BMP");
+		Assert.assertNotNull(imgBytes);
+
+		File imgBytesFile = File.createTempFile("text-render-unittest-renderNonUft-", ".bmp");
+		FileUtils.writeByteArrayToFile(imgBytesFile, imgBytes);
 	}
 }
